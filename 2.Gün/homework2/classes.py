92584d2044988d04b0d7d0d093431ecafea444cf
Aykut Coşkun <77541840+aykut-coskun@users.noreply.github.com>
2023-03-18 20:57:00 +0300

    Add files via upload

--- 2.Gün/homework2/classes.py
+++ 2.Gün/homework2/classes.py
@@ -1,1 +1,12 @@
 
+class Banka:
+    def krediBasvur(self):
+        print("Kredi başvurusu yapıldı")
+
+    def krediHesapla(self):
+        print("Hesaplar yapıldı")
+
+banka = Banka()
+banka.krediBasvur()
+
+# self = this 
